5226d1430e4bce31dcbb4a54e949126c72c50220
Shellyda <shellydabarbosa@gmail.com>
2025-06-23 16:02:38 -0300

    feat: starting hash map content

---
+++ Questions/C9 Hashing/find-duplicated-names.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+int main()
+{
+    vector<string> names = {"alice", "brad", "collin", "brad", "dylan", "kim"};
+    unordered_map<string, int> countMap;
+
+    for (string &name : names)
+    {
+        if (countMap.count(name) == 0)
+        {
+            countMap[name] = 1;
+        }
+        else
+        {
+            countMap[name]++;
+        }
+    }
+
+    return 0;
+}
